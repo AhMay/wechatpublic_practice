b38683c5e8e6a1db078c164342fead10af78818a
ahmay <meizi111082@hotmail.com>
2019-12-23 09:15:27 +0800

    update const

--- wechatutility/wechat_Const.py
+++ wechatutility/wechat_Const.py
@@ -1,5 +1,2 @@
-
-appId = 'wx349372b8988f6776'  # 测试账号的appid 和 ssecret
-appSecret = '842393f9522920ff375e3e50873c3c3c'
-# appId = 'wx4409cf14b491bef5'
-# appSecret = '5d97ccc1119b3c4e50c8a3776d7a5ada'
+appId = 'xxxx'  # 测试账号的appid 和 ssecret
+appSecret = 'xxxx'
